0b1cc0b9bae7452c82ca831901c92e7d93924f71
cccyb <chenyubo1996@gmail.com>
2019-02-01 16:09:49 +0800

    feat: 添加文章缩略图配置

---
+++ scripts/getPostThumb.js
@@ -0,0 +1,47 @@
+const getPostThumb = function(post, defaultThumb) {
+    // 1. 填写了thumb字段
+    if (post.thumb && validUrl(post.thumb)) { // 填写了thumb字段且字段地址合法
+        return post.thumb;
+    }
+
+    // url不合法 => 2
+    
+    // 2. 如果没有填写thumb字段，或者thumb字段不合法，则取文章内容中寻找是否有图片，如果有，且图片地址合法，则使用第一张
+    if (getFirstImgSrc(post.content)) {
+        return getFirstImgSrc(post.content);
+    }
+    
+    // 如果文章内没有图片，或者有图片，但是地址不合法 => 3
+
+    // 3. 如果文章内也没有图片，且启用了默认缩略图，且默认缩略图合法，则使用默认缩略图
+    if (defaultThumb && validUrl(defaultThumb)) {
+        return defaultThumb;
+    } else { // 4. 如果没有开启默认缩略图或者默认缩略图地址不合法，则使用/images/thumbs/下的初始默认随机图片
+        return randPostThumb();
+    }
+}
+
+// 获取随机的默认9张缩略图
+function randPostThumb () {
+    const prefix = '/images/thumbs/';
+    return prefix + randomInt(0, 9) + '.jpg';
+};
+
+// 校验url是否是合法，可以是外链，要符合http(s)开头，也可以是source内/images/下的以（jpg | png | jpeg |webp）结尾的图片
+function validUrl(url) {
+    const regUrl = /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*\.{1}(png|jpg|jpeg|webp)$/i;
+    const regFolder = /^\/images\/.+\.{1}(png|jpg|jpeg|webp)$/i
+    return regUrl.test(url) || regFolder.test(url);
+}
+
+function randomInt(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+}
+
+// 获取文章内容中第一张图片的src
+function getFirstImgSrc(text) {
+    const reg = /src=[\'\"]?([^\'\"]*)[\'\"]?/i;
+    return text.match(reg) && text.match(reg)[1] && validUrl(text.match(reg)[1]) && text.match(reg)[1] || false;
+}
+
+hexo.extend.helper.register('getPostThumb', getPostThumb);
